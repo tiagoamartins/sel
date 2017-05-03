27a6158d3cc55b2fb62da85124f7d402d7143b0a
Tiago A. Martins <tiagoammartins@gmail.com>
2017-05-03 16:03:29 -0300

    Add flask file upload example

---
+++ flask/upload.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+app = Flask(__name__)
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
