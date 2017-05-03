0b3597221bf0928877d4dc9db82aa1e3ebdb9526
Tiago A. Martins <tiagoammartins@gmail.com>
2017-05-03 15:50:47 -0300

    Add flask dynamic url redirection

---
+++ flask/dynamic_url.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
