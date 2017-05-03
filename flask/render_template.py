d473d50d1b3354b3fd5cf0a66098c73cff84f2cb
Tiago A. Martins <tiagoammartins@gmail.com>
2017-05-03 15:53:03 -0300

    Add flask HTML rendering and templates

---
+++ flask/render_template.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/hello/<user>')
+def hello_name(user):
+    return render_template('hello_template.html', name=user)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
