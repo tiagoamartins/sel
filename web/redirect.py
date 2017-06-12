cf6ce490037a05b09a16fcb46bd3381c6649e5d7
Tiago A. Martins <tiagoammartins@gmail.com>
2017-06-11 22:58:33 -0300

    Use reStructuredText inside docstrings

--- web/redirect.py
+++ web/redirect.py
@@ -1,12 +1,12 @@
 """
-HTTP Codes:
-    400 − for Bad Request
-    401 − for Unauthenticated
-    403 − for Forbidden
-    404 − for Not Found
-    406 − for Not Acceptable
-    415 − for Unsupported Media Type
-    429 − Too Many Requests
+HTTP Codes
+    * 400 − for Bad Request
+    * 401 − for Unauthenticated
+    * 403 − for Forbidden
+    * 404 − for Not Found
+    * 406 − for Not Acceptable
+    * 415 − for Unsupported Media Type
+    * 429 − Too Many Requests
 """
 
 from flask import Flask, redirect, url_for, render_template, request, abort
