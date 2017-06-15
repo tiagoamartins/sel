8bb7f7097372df577644e716799ff836b38128a4
Tiago A. Martins <tiagoammartins@gmail.com>
2017-06-14 23:59:13 -0300

    Fix data set creation while executing from module

--- fabrica.py
+++ fabrica.py
@@ -1,10 +1,12 @@
 import csv
 from faker import Factory
-faker = Factory.create('pt_BR')  # cria fábrica de dados falsos em pt-BR
 
-with open('dados.txt', 'w') as arq:
-    escritor = csv.writer(arq, delimiter="-", quotechar="Z")
+if __name__ == '__main__':
+    faker = Factory.create('pt_BR')  # cria fábrica de dados falsos em pt-BR
 
-    for _ in range(100):
-        dados = faker.name(), faker.job(), faker.company()
-        escritor.writerow(dados)
+    with open('dados.txt', 'w') as arq:
+        escritor = csv.writer(arq, delimiter="-", quotechar="Z")
+
+        for _ in range(100):
+            dados = faker.name(), faker.job(), faker.company()
+            escritor.writerow(dados)
