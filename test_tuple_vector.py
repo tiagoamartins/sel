e21abd10bb74322ef70a9e6670d5c01ddd4041d0
Tiago A. Martins <tiagoammartins@gmail.com>
2017-08-10 11:37:03 -0300

    Add docstrings to tuple vector test

--- test_tuple_vector.py
+++ test_tuple_vector.py
@@ -3,19 +3,34 @@ from unittest import TestCase
 from tuple_vector import Vetor, soma_vetor, subtrai_vetor
 
 
-class TestaVetor(TestCase):
-    def testa_vetor(self):
+class TestVetor(TestCase):
+    """
+    Verifica a funcionalidade da tupla ``Vetor``
+    """
+
+    def test_vetor(self):
+        """
+        Verifica a atribuição da tupla ``Vetor``
+        """
+
         v = Vetor(x=1, y=-1)
         self.assertEqual(v.x, 1)
         self.assertEqual(v.y, -1)
 
-    def testa_soma(self):
+    def test_soma(self):
+        """
+        Verifica soma de duas tuplas ``Vetor``
+        """
+
         v1 = Vetor(5, 1)
         v2 = Vetor(0, 3)
         v = soma_vetor(v1, v2)
         self.assertEqual(v, Vetor(5, 4))
 
-    def testa_subtrai(self):
+    def test_subtrai(self):
+        """
+        Verifica subtração de duas tuplas ``Vetor``
+        """
         v1 = Vetor(5, 1)
         v2 = Vetor(2, 3)
         v = subtrai_vetor(v1, v2)
